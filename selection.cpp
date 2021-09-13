f67020cc80122b4909458cff0ad6a42ef731bf47
Elkson Eller <elksoneller@gmail.com>
2021-09-13 09:45:03 -0300

    Cria regra para ignorar executaveis. Ajustes implementações dos algoritmos.

--- selection.cpp
+++ selection.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
+//pesquisar totally_ordered
+//templates e concepts
 //Sorts a given array by bubble sort
 //Input: An array A[0..n-1] of orderable elements
 //Output: Array A[0..n-1] sorted in nondecreasing order
 int main() {
   std::vector<int> v = { 89, 45, 68, 90, 29, 34, 17 };
   int min;
-  for (int i = 0; i < v.size(); i++) {
+  for (int i = 0; i < v.size() -1; i++) {
     min = i;
     for (int j = i + 1; j < v.size(); j++) {
       //std::cout << v[j] << ", ";
